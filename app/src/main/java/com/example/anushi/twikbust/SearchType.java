862e24a93fe9f36ab504d9b8b750576bf8e47f86
Anushi <anushitomar5@gmail.com>
2017-07-10 02:28:21 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/anushi/twikbust/SearchType.java
@@ -0,0 +1,11 @@
+package com.example.anushi.twikbust;
+
+/**
+ * Created by anushi on 9/7/17.
+ */
+
+public class SearchType {
+    public static int MyFollowing=1;
+    public static int OnePerson=2;
+    public static int SearchIn=3;
+}
